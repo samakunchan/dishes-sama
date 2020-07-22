fe0969d4ec45dcb0f1ea7a3cb2e36e918943b2cd
samakunchan <badgehades@hotmail.com>
2020-07-22 06:33:41 +0200

    feat: routing, meta, préparation
    - Ajout du routing en lazing loading.
    - Ajout des balises meta tout de suite, au moins c'est fait.
    - Préparation à la TDD.
    - Ajout style scss pour le header en design BEM.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dish-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'dishes-sama';
+  constructor() {}
 }
