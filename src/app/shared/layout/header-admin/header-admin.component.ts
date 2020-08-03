b9059de72530bd1622ecd93917e450d72be8e61e
samakunchan <badgehades@hotmail.com>
2020-08-03 18:37:06 +0200

    feat: ajout du header-admin.component

---
+++ src/app/shared/layout/header-admin/header-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dish-header-admin',
+  templateUrl: './header-admin.component.html',
+  styleUrls: ['./header-admin.component.scss'],
+})
+export class HeaderAdminComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
